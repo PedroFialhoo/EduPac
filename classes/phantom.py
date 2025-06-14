4e1b730101a17ce04d7b4c3a06d68fa8557ca2ef
Pedro Fialho <pedrofialho000@gmail.com>
2025-06-14 10:06:34 -0300

    feat: create Phantom and Game classes, refactor main, and organize files into packages

---
+++ classes/phantom.py
@@ -0,0 +1,16 @@
+import random
+
+class Phantom:
+    def __init__(self):
+        pass
+    
+    def showPhantom(self):
+        pass
+    
+    def walk(self):
+        pass
+    
+    def damage(self):
+        pass
+    
+    
